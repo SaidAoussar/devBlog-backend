2a474c6ab2af0b50da5e257a9bd6c151463951fb
SaidAoussar <aoussarsaid33@gmail.com>
2022-08-02 16:55:18 +0100

    add validation to create post dto to validate body of request

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  @ApiProperty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(100)
+  @ApiProperty()
   content: string;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({ required: false, default: false })
   published: boolean;
 }
